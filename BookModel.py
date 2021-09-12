56f0277639fad7375f06900e6f6c930b3b0dbd1b
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2021-09-12 01:23:06 +0100

    Setting up the table and columns

--- BookModel.py
+++ BookModel.py
@@ -3,3 +3,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import json
 from settings import app
+
+# Creating the object of the database class
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    # Defining the table name
+    __tablename__ = 'books'
+
+    # Defining columns
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False)
+    price = db.Column(db.Float, nullable = False)
+    isbn = db.Column(db.Integer)
